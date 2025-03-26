818db96b8217b7c851582d9642f8736b6f071bc8
Julien <julien.berthoud@gematik.de>
2025-03-26 22:35:46 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PixiSkyComponent} from './pixi-sky/pixi-sky.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PixiSkyComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
